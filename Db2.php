089230d818c0f011cf2c089f3efbf91cd821acf4
Ariel Toure <arielz360007@gmail.com>
2025-12-17 09:32:29 +0000

    Fix database connection host in Db2.php

--- Db2.php
+++ Db2.php
@@ -1,13 +1,14 @@
 <?php
 //Coordonnées du serveur sql
 $Dbname="choose";
+$Host='127.0.0.8';
 $Port=3310;
 $Username="root";
 $Password="Ariel&007";
 
 //Connexion à la base de donnée
 try{
-  $Connect= new PDO('mysql:host= dbname=$Dbname, port=$Port', $Username, $Password);
+  $Connect= new PDO('mysql:host=$Host, dbname=$Dbname, port=$Port', $Username, $Password);
   $Connect-> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXECPTION);
 
 
